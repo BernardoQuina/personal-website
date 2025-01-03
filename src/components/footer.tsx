8c935fb7a38228185166bc5f642c65b75efcc8a4
Bernardo Quina <bquina@primeit.pt>
2025-01-03 14:05:37 +0000

    Simpler footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,31 +1,23 @@
-import Link from 'next/link';
 import { Globe, Github } from 'lucide-react';
 
 import { Aws } from './icons/aws';
 import { Linkedin } from './icons/linkedin';
 import { X } from './icons/x';
-import { Button } from './ui/button';
 
 export function Footer() {
   return (
     <footer className="mt-20 w-full items-center overflow-hidden bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="w-full max-w-[70rem] justify-between gap-12 px-4 py-12 sm:flex-row sm:gap-0 lg:px-8">
         <div className="w-full flex-row items-center justify-around sm:contents sm:justify-between">
-          <div className="gap-4">
-            <Link href="/" className="justify-center">
-              <h1 className="text-xs font-bold">BERNARDO QUINA</h1>
-            </Link>
-            <div>
+          <div className="justify-center">
+            <div className="flex-column items-center gap-1 sm:items-start">
+              <p className="text-sm text-muted-foreground">
+                Website deployed in
+              </p>
               <div className="flex-row gap-1">
-                <p className="text-sm text-muted-foreground">
-                  Website deployed in
-                </p>
+                <p className="text-sm text-muted-foreground">and powered by</p>
                 <Aws className="h-7 w-7" />
               </div>
-              <p className="-mt-1.5 text-center text-sm text-muted-foreground sm:text-start">
-                Bernardo Quina <br />
-                Lisbon, Portugal
-              </p>
             </div>
           </div>
           <div className="items-center gap-4 self-start">
@@ -72,20 +64,11 @@ export function Footer() {
             </div>
           </div>
         </div>
-        <div className="items-center sm:items-end sm:pl-6">
-          <div className="flex-row gap-4 sm:contents sm:gap-0">
-            <Button variant="link" asChild className="w-fit px-0">
-              <Link href="/privacy">
-                <span className="text-sm font-medium">Privacy Policy</span>
-              </Link>
-            </Button>
-            <Button variant="link" asChild className="w-fit px-0">
-              <Link href="/terms">
-                <span className="text-sm font-medium">Terms of Service</span>
-              </Link>
-            </Button>
-          </div>
-          <span className="pt-2 text-sm text-muted-foreground">
+        <div className="items-center justify-center gap-1 sm:items-end">
+          <span className="text-sm text-muted-foreground">
+            Lisbon, Portugal
+          </span>
+          <span className="text-sm text-muted-foreground">
             © {new Date().getFullYear()} Bernardo Quina
           </span>
         </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,9 @@
-import Link from 'next/link';
-
 import { ThemeToggle } from './theme-toggle';
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 h-14 w-full items-center justify-center border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border">
-      <div className="w-full max-w-[70rem] flex-row justify-between px-4 lg:px-8">
-        <Link href="/" className="justify-center">
-          <h1 className="text-sm font-bold">BERNARDO QUINA</h1>
-        </Link>
+    <header className="sticky top-0 z-50 h-14 w-full items-center justify-center bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="w-full max-w-[70rem] flex-row justify-end px-4 lg:px-8">
         <div className="flex-row gap-2">
           <ThemeToggle />
         </div>

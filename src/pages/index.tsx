f6743d065d3883126e11a3447fe4ec59e4016474
Bernardo Quina <bquina@primeit.pt>
2025-01-16 13:29:17 +0000

    Move to fixed elements for better control

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { motion, useScroll, useTransform } from 'motion/react';
 
 import { OrbiterEffect } from '../components/orbiter';
 import { PageLayout } from '../components/page-layout';
@@ -8,68 +9,86 @@ import { K8s } from '../components/icons/k8s';
 import { React } from '../components/icons/react';
 
 export default function Home() {
+  const { scrollY } = useScroll();
+
+  const developerScale = useTransform(scrollY, [0, 200], [1, 2]);
+
+  const profileOpacity = useTransform(scrollY, [0, 200], [1, 0]);
+  const profileY = useTransform(scrollY, [0, 200], [0, -200]);
+
   return (
     <PageLayout pageTitle="Bernardo Quina" pageDescription="todo">
-      <div className="items-center justify-center">
-        <div className="min-h-[calc(100vh-3.5rem)] items-center justify-center">
-          <div className="relative mt-auto">
-            <OrbiterEffect initialPosition={90} rotate={45}>
-              <Aws className="-rotate-45" />
-            </OrbiterEffect>
-            <OrbiterEffect
-              className="mt-[-300px]"
-              initialPosition={270}
-              rotate={45}
-            >
-              <K8s className="-rotate-45" />
-            </OrbiterEffect>
-            <OrbiterEffect className="mt-[-300px]" rotate={135}>
-              <NodeJS className="rotate-[-135deg]" />
-            </OrbiterEffect>
-            <OrbiterEffect
-              className="mt-[-300px]"
-              rotate={135}
-              initialPosition={180}
-            >
-              <React className="rotate-[-135deg]" />
-            </OrbiterEffect>
-            <div
-              className="absolute left-[50%] top-[50%] -z-10 h-[300px] w-[300px] translate-x-[-52%] translate-y-[-50%] transform rounded-full bg-orange-600 blur-xl"
-              style={{
-                background:
-                  'radial-gradient(rgba(255,165,0,1) 30%, rgba(255,165,0,0.3) 31%, rgba(255,165,0,0.15) 50%, rgba(255,165,0,0) 100%)',
-              }}
-            />
+      <motion.div className="fixed">{scrollY}</motion.div>
+      <motion.div
+        className="fixed left-[50%] top-20 items-center"
+        style={{ opacity: profileOpacity, x: '-50%', y: profileY }}
+      >
+        <div className="relative">
+          <OrbiterEffect initialPosition={90} rotate={45}>
+            <Aws className="-rotate-45" />
+          </OrbiterEffect>
+          <OrbiterEffect
+            className="mt-[-300px]"
+            initialPosition={270}
+            rotate={45}
+          >
+            <K8s className="-rotate-45" />
+          </OrbiterEffect>
+          <OrbiterEffect className="mt-[-300px]" rotate={135}>
+            <NodeJS className="rotate-[-135deg]" />
+          </OrbiterEffect>
+          <OrbiterEffect
+            className="mt-[-300px]"
+            rotate={135}
+            initialPosition={180}
+          >
+            <React className="rotate-[-135deg]" />
+          </OrbiterEffect>
+          <div
+            className="absolute left-[50%] top-[50%] -z-10 h-[300px] w-[300px] translate-x-[-52%] translate-y-[-50%] rounded-full bg-orange-600 blur-xl"
+            style={{
+              background:
+                'radial-gradient(rgba(255,165,0,1) 30%, rgba(255,165,0,0.3) 31%, rgba(255,165,0,0.15) 50%, rgba(255,165,0,0) 100%)',
+            }}
+          />
 
-            <div className="absolute left-[50%] top-[50%] z-0 h-[220px] w-[220px] translate-x-[-50%] translate-y-[-50%] transform">
-              <div className="overflow-hidden rounded-2xl border border-orange-300/50 bg-orange-200/70 shadow-lg backdrop-blur-[2px] supports-[backdrop-filter]:bg-orange-200/70">
-                <Image
-                  src="/images/profile-picture.png"
-                  alt="Bernardo Quina's profile picture"
-                  height={220}
-                  width={220}
-                  priority
-                />
-              </div>
+          <div className="absolute left-[50%] top-[50%] z-0 h-[220px] w-[220px] translate-x-[-50%] translate-y-[-50%]">
+            <div className="overflow-hidden rounded-2xl border border-orange-300/50 bg-orange-200/70 shadow-lg backdrop-blur-[2px] supports-[backdrop-filter]:bg-orange-200/70">
+              <Image
+                src="/images/profile-picture.png"
+                alt="Bernardo Quina's profile picture"
+                height={220}
+                width={220}
+                priority
+              />
             </div>
           </div>
-          <div className="w-[220px]">
-            <span className="text-sm font-medium">Hey there 👋 , I&apos;m</span>
-            <h2 className="text-3xl font-medium">Bernardo Quina</h2>
-            <button className="self-end pt-1">
-              <h1 className="text-sm font-medium">
-                {'<FullStackDeveloper />'}
-              </h1>
-            </button>
-          </div>
-          <div className="mt-auto items-center gap-2">
-            <span className="text-[10px] font-bold tracking-[0.2rem] text-gray-400 dark:text-gray-700">
-              SCROLL
-            </span>
-            <div className="h-[30vh] w-[0.75px] bg-gray-300 dark:bg-gray-800 sm:h-[20vh]"></div>
-          </div>
         </div>
-      </div>
+        <div className="w-[220px]">
+          <span className="text-sm font-medium">Hey there 👋 , I&apos;m</span>
+          <h2 className="text-3xl font-medium">Bernardo Quina</h2>
+        </div>
+      </motion.div>
+      <motion.div
+        className="fixed bottom-0 left-[50%] translate-x-[-50%] items-center gap-2"
+        style={{ opacity: profileOpacity }}
+      >
+        <span className="text-[10px] font-bold tracking-[0.2rem] text-gray-400 dark:text-gray-600">
+          SCROLL
+        </span>
+        <div className="h-[30vh] w-[0.75px] bg-gray-300 dark:bg-gray-700 sm:h-[20vh]" />
+      </motion.div>
+      <motion.button
+        className="fixed left-[50%] top-[27.5rem]"
+        style={{ scale: developerScale, x: -45 }}
+      >
+        <h1 className="text-sm font-medium">
+          <span className="text-gray-400">{'<'}</span>
+          FullStackDeveloper
+          <span className="text-gray-400">{' />'}</span>
+        </h1>
+      </motion.button>
+      <div className="h-[5000vh]" />
     </PageLayout>
   );
 }

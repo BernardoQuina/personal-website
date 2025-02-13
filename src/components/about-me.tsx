4529c06edae53129c6c40188ac5100a4e9e0779f
Bernardo Quina <66028036+BernardoQuina@users.noreply.github.com>
2025-02-13 16:23:55 +0000

    minor text update

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -224,7 +224,7 @@ function AboutMeContent({ scrollYProgress, contentRef }: Props) {
         style={{ scale, x, y, opacity }}
       >
         <p className="text-muted-foreground">
-          I&apos;m a Full Stack Developer with 3 years of experience
+          I&apos;m a Full Stack Developer with more than 3 years of experience
           specializing in
           <span className="font-medium text-foreground"> TypeScript </span>and
           cloud architecture. Proven track record delivering end to end
